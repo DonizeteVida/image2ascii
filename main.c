ff8af9a5185d769d70ef9fc5e71c2f226662289e
Donizete Vida <donizetevida1998@outlook.com>
2022-09-11 10:36:53 -0300

    Update afro

--- main.c
+++ main.c
@@ -9,9 +9,9 @@
 
 int main(int argc, char *args[]) {
     char* filename = "./demo/afro.jpg";
-    Image* image = scaleImage(raw2Image(getRaw(filename)), 2);
-    toHtml("demo/afro.html", image, '@', 2, "black");
-    toGrayHtml("demo/afro_gray.html", image, '@', 2, "black");
+    Image* image = scaleImage(raw2Image(getRaw(filename)), .3);
+    toHtml("demo/afro.html", image, '@', 1, "black");
+    toGrayHtml("demo/afro_gray.html", image, '@', 1, "black");
     toAscii("demo/afro.txt", image);
     free(image->pixels);
     free(image);
